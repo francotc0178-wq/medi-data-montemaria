13d5bb6e47ec48f5afe895e6532f24132b596db7
francotc0178-wq <francotc0178@gmail.com>
2026-09-10 01:34:15 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const multer = require('multer');
 const nodemailer = require('nodemailer');
-const cors = require('50mb');
+const cors = require('cors');
 const path = require('path');
 
 const app = express();
